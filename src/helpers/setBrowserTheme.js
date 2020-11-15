f55711c5224f2591296e28adc5bdc6cc83171a34
Fabiano <fabinppk@hotmail.com>
2020-11-15 19:56:34 -0300

    fix: set secndary color from weather

--- src/helpers/setBrowserTheme.js
+++ src/helpers/setBrowserTheme.js
@@ -3,11 +3,12 @@ export const setThemeColor = () => {
     const el = document.querySelectorAll('[data-testid="weather"');
     if (!el[0]) return;
     const actualTypeColor = window.getComputedStyle(el[0], null).getPropertyValue('background');
-    const theme = `${actualTypeColor.split(',')[4]},${actualTypeColor.split(',')[5]},${
-        actualTypeColor.split(',')[6]
+    const theme = `${actualTypeColor.split(',')[7]},${actualTypeColor.split(',')[8]},${
+        actualTypeColor.split(',')[9]
     }`;
-    console.log(theme);
-    document.querySelectorAll('meta[name="theme-color"')[0].setAttribute('content', theme);
+    document
+        .querySelectorAll('meta[name="theme-color"')[0]
+        .setAttribute('content', theme.substr(0, 18));
 };
 
 export default {
